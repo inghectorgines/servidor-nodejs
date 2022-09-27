218965f24212a468d2e9b373c15d2a05b413b3a2
inghectorgines <ing.hectorgines@live.com>
2022-09-26 21:25:21 -0500

    Server js 2 -  26 de sep 9:25pm

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 80;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Bienvenido al Servidor JS de TMD');
 });
 
 server.listen(port, () => {
